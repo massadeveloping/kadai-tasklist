3ddfa646bb8c87fd62683aa3d9bb30a1b69931ab
massadeveloping <i_c_2635_cabinet_noir@yahoo.co.jp>
2017-11-18 08:49:24 +0000

    add column

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>tasklist</title>
+    </head>
+    
+    <body>
+        @include("commons.error_messages")
+        @yield("content")
+    </body>
+</html>
